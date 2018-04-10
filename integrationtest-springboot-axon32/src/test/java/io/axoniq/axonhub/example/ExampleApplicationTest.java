4ad9ef7ec8c0c46b3720a932daf403c60814a528
Allard Buijze <allard.buijze@axoniq.io>
2018-04-10 10:37:41 +0200

    Ensure AxonHub Spring Boot Starter works in Axon 3.1
    With Axon 3.1, the autoconfiguration will not create a QueryBus
    implementation, as it is not compatible with the interface defined
    in 3.1.

---
+++ integrationtest-springboot-axon32/src/test/java/io/axoniq/axonhub/example/ExampleApplicationTest.java
@@ -0,0 +1,25 @@
+package io.axoniq.axonhub.example;
+
+import io.axoniq.axonhub.client.query.AxonHubQueryBus;
+import org.axonframework.queryhandling.QueryBus;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ExampleApplicationTest {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    public void testApplicationStartsWithAxon31() {
+        assertEquals(AxonHubQueryBus.class, applicationContext.getBean(QueryBus.class).getClass());
+    }
+}
